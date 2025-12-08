a5f51e5d339227eb132f06889f93412c2d25c377
kul349 <karkikulbahadur344@gmail.com>
2025-12-08 11:49:56 +0545

    installtion of all library

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="bg-red-300 ">
+      <p className="text-xl font-bold ">hello world</p>
+      <p>hellow word i am here with someone to whom i can udnerstand </p>
+    </div>
+  );
+}
+
+export default App;
