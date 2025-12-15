2d88336774dfb8b24b15eee897fd0968d08867a1
kul349 <karkikulbahadur344@gmail.com>
2025-12-15 11:27:52 +0545

    changing some thing in tailwindcss config

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 import Header from "./Header";
 function MainLayout({children}) {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header/>
-      <main className="flex-grow">{children}</main>
+    <div className="min-h-screen flex flex-col overflow-x-hidden">
+      <Header />
+      <main className="flex-grow px-4 sm:px-6 lg:px-8">{children}</main>
     </div>
   );
 }
