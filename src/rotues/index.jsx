bf397793fa2c7ca03e12f2d491deac449e3241c0
kul349 <karkikulbahadur344@gmail.com>
2025-12-17 11:38:08 +0545

    adding the correct code

--- src/rotues/index.jsx
+++ src/rotues/index.jsx
@@ -6,8 +6,7 @@ import HomePage from "../pages/HomePage";
 import TestDetailPage from "../pages/TestDetailPage";
 import AssessmentStagesPage from "../pages/AssessmentStagesPage";
 
-
-function AppRoutes(){
+function AppRoutes() {
   const location = useLocation();
 
   useEffect(() => {
@@ -19,52 +18,52 @@ function AppRoutes(){
       });
     }
   }, [location]);
-    return (
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <MainLayout>
-              <HomePage />
-            </MainLayout>
-          }
-        />
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <MainLayout>
+            <HomePage />
+          </MainLayout>
+        }
+      />
 
-        <Route
-          path="/page/test-details"
-          element={
-            <MainLayout>
-              <TestDetailPage />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="/page/assesment-stage"
-          element={
-            <MainLayout>
-              <AssessmentStagesPage />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="*"
-          element={
-            <MainLayout>
-              <div className="container mx-auto p-8 text-center">
-                <h1 className="text-4xl font-bold text-red-600">404</h1>
-                <p className="text-xl mt-4">Page Not Found</p>
-                <a
-                  href="/"
-                  className="mt-6 inline-block bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
-                >
-                  Go Home
-                </a>
-              </div>
-            </MainLayout>
-          }
-        />
-      </Routes>
-    );
+      <Route
+        path="/page/test-details"
+        element={
+          <MainLayout>
+            <TestDetailPage />
+          </MainLayout>
+        }
+      />
+      <Route
+        path="/page/assesment-stage"
+        element={
+          <MainLayout>
+            <AssessmentStagesPage />
+          </MainLayout>
+        }
+      />
+      <Route
+        path="*"
+        element={
+          <MainLayout>
+            <div className="container mx-auto p-8 text-center">
+              <h1 className="text-4xl font-bold text-red-600">404</h1>
+              <p className="text-xl mt-4">Page Not Found</p>
+              <a
+                href="/"
+                className="mt-6 inline-block bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+              >
+                Go Home
+              </a>
+            </div>
+          </MainLayout>
+        }
+      />
+    </Routes>
+  );
 }
 
-export default AppRoutes
+export default AppRoutes;
