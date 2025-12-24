160cd1abf657fb06021363d6111146e51c3fc831
kul349 <karkikulbahadur344@gmail.com>
2025-12-24 09:50:36 +0545

    adding the vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { prerender } from "vite-plugin-prerender";
+import prerender from "vite-plugin-prerender";
 
 export default defineConfig({
   plugins: [
