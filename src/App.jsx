63741c2b33dc45ee2881edaeca1348b8a1b54bdf
kul349 <karkikulbahadur344@gmail.com>
2025-12-28 23:41:37 +0545

    adding the file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async"; 
+import { HelmetProvider } from "react-helmet-async";
 import "./index.css";
 import AppRoutes from "./rotues";
 
 function App() {
+  useEffect(() => {
+   
+    document.dispatchEvent(new Event("custom-render-trigger"));
+  }, []);
+
   return (
     <HelmetProvider>
       <Router>
